9336fc84d0bed34a6a7aba8bacf97796319f3558
kylelyk <kylelyk@gmail.com>
2016-04-27 14:28:29 -0600

    Improved types internal to Aligner.

--- src/Aligner.cpp
+++ src/Aligner.cpp
@@ -9,10 +9,20 @@
 
 using namespace std;
 
-typedef list<tuple<int, int, int>> mr_list;
-typedef vector<pair<mr_list, int>> car_list;
+struct match_region{
+    long diagonal;
+    unsigned int read_pos;
+    unsigned int seq_pos;
+    unsigned int length;
+};
 
-Aligner::Aligner(int kmer_length, const string &reference):
+struct candidate_mr{
+    list<match_region> mr_list;
+    unsigned int matched_bases;
+
+};
+
+Aligner::Aligner(unsigned int kmer_length, const string &reference):
     kmer_length(kmer_length),
     reference(reference),
     index(kmer_length, reference){}
@@ -21,64 +31,64 @@ pair<GappedSequence, GappedSequence> Aligner::find_alignment(const string &read)
     return find_alignment(read, this->kmer_length);
 }
 
-pair<GappedSequence, GappedSequence> Aligner::find_alignment(const string &read, int kmer_length){
+pair<GappedSequence, GappedSequence> Aligner::find_alignment(const string &read, unsigned int kmer_length){
     if(kmer_length < this->kmer_length){
         cerr << "Warning: 0 results will be returned when finding an alignment using kmer sizes less than what was specfied when building the hybrid index.\n";
     }
 
     //Contains a list of diagonal, kmer read position pairs
-    vector<pair<int, int>> matching_regions;
+    vector<match_region> matching_regions;
     for(unsigned int i = 0; i < read.length() - kmer_length + 1; i++){
         cout << read.substr(i, kmer_length) << endl;
         cout << index.query(read.substr(i, kmer_length)).size() << " results\n";
-        for(int elm : index.query(read.substr(i, kmer_length))){
-            matching_regions.push_back(pair<int, int>(elm-i, i));
+        for(unsigned int elm : index.query(read.substr(i, kmer_length))){
+            matching_regions.push_back({elm-i, i, elm, kmer_length});
         }
     }
 
     //Sort MR's by diagonal then read position
     sort(begin(matching_regions), end(matching_regions),
-        [](const pair<int, int> a, const pair<int, int> b){
+        [](const match_region &a, const match_region &b){
             //compare using the ints as indices into the sequence
-            if(a.first == b.first){
-                return a.second < b.second;
+            if(a.diagonal == b.diagonal){
+                return a.read_pos < b.read_pos;
             }else{
-                return a.first < b.first;
+                return a.diagonal < b.diagonal;
             }
         });
 
     for(auto elm : matching_regions){
-        cout << elm.first << " " << elm.second << endl;
+        cout << elm.diagonal << " " << elm.read_pos << " " << elm.seq_pos << " " << elm.length << endl;
     }
 
     //Merge MR's if they have identical diagonal values and successive offsets
-    //diagonal, read position, length
-    mr_list new_mrs;
-    int cur_diagonal = matching_regions.front().first;
-    int cur_pos = matching_regions.front().second;
-    int start_pos = cur_pos;
-    int cur_length = kmer_length;
+    list<match_region> new_mrs;
+    int cur_diagonal = matching_regions.front().diagonal;
+    unsigned int cur_pos = matching_regions.front().read_pos;
+    unsigned int seq_pos = matching_regions.front().seq_pos;
+    unsigned int start_pos = cur_pos;
+    unsigned int cur_length = kmer_length;
     for(auto elm : matching_regions){
         //Add merged MR's
-        if(elm.first != cur_diagonal || elm.second != cur_pos){
-            new_mrs.push_back(make_tuple(cur_diagonal, start_pos, cur_length-1));
-            cur_diagonal = elm.first;
-            cur_pos = elm.second;
+        if(elm.diagonal != cur_diagonal || elm.read_pos != cur_pos){
+            new_mrs.push_back({cur_diagonal, start_pos, seq_pos, cur_length-1});
+            cur_diagonal = elm.diagonal;
+            cur_pos = elm.read_pos;
             start_pos = cur_pos;
-            cur_length = kmer_length;
+            cur_length = elm.length;
+            seq_pos = elm.seq_pos;
         }
         cur_pos++;
         cur_length++;
     }
 
     //Add remaining MR
-    new_mrs.push_back(make_tuple(cur_diagonal, start_pos, cur_length-1));
+    new_mrs.push_back({cur_diagonal, start_pos, seq_pos, cur_length-1});
 
-    for(auto elm : new_mrs){
-        cout << "(" << get<0>(elm) << " " << get<1>(elm) << " " << get<2>(elm) << ") ";
+    cout << "New matching regions.";
+    for(auto elm : matching_regions){
+        cout << elm.diagonal << " " << elm.read_pos << " " << elm.seq_pos << " " << elm.length << endl;
     }
-
-    cout << endl;
 
 
     //max_adjacency allowed for combining MR's into CAR's
@@ -86,40 +96,62 @@ pair<GappedSequence, GappedSequence> Aligner::find_alignment(const string &read,
     int max_adjacency = 2;//0.1 * read.size();
 
     //Compute CAR's by merging MR's with diagonals with a different <= max_adjacency
-    vector<pair<list<tuple<int, int, int>>, int>> candidate_regions;
-    cur_diagonal = get<0>(new_mrs.front());
-    int cur_match = 0;
-    mr_list l;
+    vector<candidate_mr> candidate_regions;
+    cur_diagonal = new_mrs.front().diagonal;
+    unsigned int cur_match = 0;
+    list<match_region> l;
     for(auto elm : new_mrs){
         //Add merged MR's
-        if(abs(get<0>(elm) - cur_diagonal) <= max_adjacency){
-            cout << "mr with diag " << get<0>(elm) << " is in range of cur diag " << cur_diagonal << endl;
+        if(abs(elm.diagonal - cur_diagonal) <= max_adjacency){
+            cout << "mr with diag " << elm.diagonal << " is in range of cur diag " << cur_diagonal << endl;
             l.push_back(elm);
-            cur_match += kmer_length - abs(cur_diagonal - get<0>(elm));
+            cur_match += kmer_length - abs(cur_diagonal - elm.diagonal);
         }else{
-            cout << "mr with diag " << get<0>(elm) << " is not in range of cur diag " << cur_diagonal << endl;
-            candidate_regions.push_back(pair<mr_list, int>(l, cur_match));
+            cout << "mr with diag " << elm.diagonal << " is not in range of cur diag " << cur_diagonal << endl;
+            candidate_regions.push_back({l, cur_match});
             l.clear();
             l.push_back(elm);
-            cur_diagonal = get<0>(elm);
-            cur_match = get<2>(elm);
+            cur_diagonal = elm.diagonal;
+            cur_match = elm.length;
         }
     }
 
-    candidate_regions.push_back(pair<mr_list, int>(l, cur_match));
+    candidate_regions.push_back({l, cur_match});
 
     //Sort CAR's
     sort(begin(candidate_regions), end(candidate_regions),
-        [](const pair<mr_list, int> a, const pair<mr_list, int> b){
-            return a.second > b.second;
+        [](const candidate_mr &a, const candidate_mr &b){
+            return a.matched_bases > b.matched_bases;
         });
 
     for(auto car : candidate_regions){
-        cout << car.second << " matches:\n";
-        for(auto mr : car.first){
-            cout << get<0>(mr) << " " << get<1>(mr) << " " << get<2>(mr) << endl;
+        cout << car.matched_bases << " matches:\n";
+        for(auto elm : car.mr_list){
+            cout << elm.diagonal << " " << elm.read_pos << " " << elm.seq_pos << " " << elm.length << endl;
         }
     }
 
+    //Align each candidate region
+    for(auto cr : candidate_regions){
+        //Align LMUR (Left-Most Unmatched Region)
+        match_region mr = cr.mr_list.front();
+
+        //Only do mismatch, insertion, and deletion search up to this many characters
+        int search_limit = kmer_length;
+        //Detect Mismatch case
+        // int start_seq = cr.second[0].get<0>()
+        // for(int i = 0; i < search_limit; i++){
+            
+        // }
+
+        //Mixed case
+        global_align(reference, read, pair<int, int>(mr.seq_pos - search_limit, mr.seq_pos), pair<int, int>(0, mr.read_pos));
+    }
+
+
     return pair<GappedSequence,GappedSequence>(GappedSequence(read), GappedSequence(read));
+}
+
+pair<GappedSequence,GappedSequence> Aligner::global_align(const string &seq1, const string &seq2, pair<int, int> seq1_range, pair<int, int> seq2_range){
+    return pair<GappedSequence,GappedSequence>(seq1, seq2);
 }
--- src/Aligner.h
+++ src/Aligner.h
@@ -8,13 +8,14 @@ using namespace std;
 
 class Aligner{
     public:
-        Aligner(int kmer_length, const string &reference);
+        Aligner(unsigned int kmer_length, const string &reference);
         pair<GappedSequence, GappedSequence> find_alignment(const string &read);
-        pair<GappedSequence, GappedSequence> find_alignment(const string &read, int kmer_length);
+        pair<GappedSequence, GappedSequence> find_alignment(const string &read, unsigned int kmer_length);
     private:
-        int kmer_length;
+        unsigned int kmer_length;
         string reference;
         HybridIndex index;
+        pair<GappedSequence,GappedSequence> global_align(const string &seq1, const string &seq2, pair<int, int> seq1_range, pair<int, int> seq2_range);
 };
  
 #endif
